feea89745c2a9bcca4563dcb3abe74c08aebba58
raesga02 <98980827+raesga02@users.noreply.github.com>
2023-10-16 16:13:44 +0200

    Add files via upload

--- Assignment.py
+++ Assignment.py
@@ -1,6 +1,13 @@
 from pprint import pprint 
+from time import sleep
 from DbConnector import DbConnector
+from Queries import QueriesLibrary
+from tabulate import tabulate
 
+import os
+import csv
+import sys
+import argparse
 
 class Geolife:
 
@@ -8,6 +15,7 @@ class Geolife:
         self.connection = DbConnector()
         self.client = self.connection.client
         self.db = self.connection.db
+        self.query_library = QueriesLibrary(self.db)
 
     def create_coll(self, collection_name):
         collection = self.db.create_collection(collection_name)    
@@ -56,21 +64,40 @@ class Geolife:
         collections = self.client['test'].list_collection_names()
         print(collections)
          
-
+def argument_parser():
+    parser = argparse.ArgumentParser(description='Geolife database manager')
+    parser.add_argument('-i', '--initialize', action='store_true', help='Initialize data into database')
+    parser.add_argument('-f', '--fetch_all', action='store_true', help='Fetch data from database')
+    parser.add_argument('-d', '--drop_all', action='store_true', help='Drop tables from database')
+    parser.add_argument('-q', '--query', type=int, help='Query number to execute')
+    return parser.parse_args()
 
 def main():
     program = None
     try:
         program = Geolife()
-        program.create_coll(collection_name="Person")
-        program.show_coll()
-        program.insert_documents(collection_name="Person")
-        program.fetch_documents(collection_name="Person")
-        program.drop_coll(collection_name="Person")
-        # program.drop_coll(collection_name='person')
-        # program.drop_coll(collection_name='users')
-        # Check that the table is dropped
-        program.show_coll()
+        args = argument_parser()
+
+        if args.initialize:
+            pass
+            # program.create_tables()
+            # program.insert_data()
+
+        if args.fetch_all:
+            pass
+            # _ = program.fetch_data(table_name="User")
+            # _ = program.fetch_data(table_name="Activity")
+            # _ = program.fetch_data_batch(table_name="TrackPoint")
+
+        if args.query:
+            program.query_library.queries[args.query]()
+
+        if args.drop_all:
+            pass
+            # program.drop_table(table_name="TrackPoint")
+            # program.drop_table(table_name="Activity")
+            # program.drop_table(table_name="User")
+        
     except Exception as e:
         print("ERROR: Failed to use database:", e)
     finally:
