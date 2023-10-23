d83f2d8902c52393760533322a43079d972968e7
Saky23s <curtur666@gmail.com>
2023-10-23 11:46:11 +0200

    Query 9 fixed and working

--- Queries.py
+++ Queries.py
@@ -81,6 +81,31 @@ class QueriesLibrary():
         Find the top 20 users with the highest number of activities.
         '''
         print("Query 3: ")
+        top_users = self.users.aggregate([
+            # Get the number of activities of each user
+            {
+                '$project': {
+                    'activity_amount': { '$size': '$activities'},
+                    'user_id': '$_id'
+                }
+
+            },
+            # Order the list by the number of activities
+            {
+                '$sort': {
+                    'activity_amount': -1
+                }
+            },
+            # Limit the output to only 20 documents
+            {
+                '$limit': 20
+            },
+            {
+                '$unset': '_id'
+            }
+        ])
+
+        for result in top_users: pprint(result)
 
 
     def query_4(self):
@@ -145,9 +170,115 @@ class QueriesLibrary():
     def query_6(self):
         '''
         a) Find the year with the most activities
-        b) Is this also the year with most recorder hours?
+        b) Is this also the year with most recorded hours?
         '''
         print("Query 6: ")
+
+        # a) Find the year with most activities
+        year_most_activities = self.activities.aggregate([
+            # Extract the year of each activity
+            {
+                '$project': {
+                    'year': {
+                        '$year': {
+                            '$dateFromString': {
+                                'dateString': '$start_date_time',
+                                'format': '%Y-%m-%d %H:%M:%S'
+                            }
+                        }
+                    }
+                }
+            },
+            # Count the number of activities for each year
+            {
+                '$group': {
+                    '_id': '$year',
+                    'num_activities': { '$sum': 1 }
+                }
+            },
+            # Sort the years by descending number of activities
+            {
+                '$sort': {
+                    'num_activities': -1
+                }
+            },
+            # Show only 1 document (the year with the most activities)
+            {
+                '$limit': 1
+            },
+            {
+                '$project': {
+                    'year': '$_id',
+                    'num_activities': '$num_activities'
+                }
+            },
+            {
+                '$unset': '_id'
+            }
+        ])
+
+        # Find the year with most recorded hours
+        year_most_recorded_hours = self.trackpoints.aggregate([
+            {
+                '$project': {
+                    'date': {
+                        '$dateFromString': {
+                            'dateString': '$date_time',
+                            'format': '%Y-%m-%d %H:%M:%S'
+                        }
+                    }
+                }
+            },
+            # Extract the year, day of the year and hour for each trackpoint
+            {
+                '$project': {
+                    'year': {
+                        '$year': '$date'
+                    },
+                    'day_of_year' : {
+                        '$dayOfYear': '$date'
+                    },
+                    'hour' : {
+                        '$hour': '$date'
+                    }
+                },
+            },
+            # Group the trackpoints by year-dayofyear-hour (disregard the hours)
+            {
+                '$group': {
+                    '_id': { 'year': '$year', 'day_of_year': '$day_of_year', 'hour': '$hour'}
+                }
+            },
+            # Count the amount of recorded hours for each group
+            {
+                '$group': {
+                    '_id': '$_id.year',
+                    'num_recorded_hours': { '$sum': 1 },
+                }
+            },
+            # Sort the result by descending value of the sum
+            {
+                '$sort': {
+                    'num_recorded_hours': -1
+                }
+            },
+            # Show only one
+            {
+                '$limit': 1
+            },
+            {
+                '$project': {
+                    'year': '$_id',
+                    'num_recorded_hours': '$num_recorded_hours'
+                }
+            },
+            {
+                '$unset': '_id'
+            }
+        ])
+
+        for result in year_most_activities: pprint(result)
+        for result in year_most_recorded_hours: pprint(result)
 
 
     def query_7(self):
@@ -292,6 +423,68 @@ class QueriesLibrary():
         where the timestamps deviate with at least 5 minutes.
         """
         print("Query 9: ")
+        activities_with_date = self.activities.aggregate([
+            #Join activities with its trackpoints
+            {
+                '$lookup':
+                {
+                    'from':'Trackpoints',
+                    'localField':'trackpoints',
+                    'foreignField':'_id',
+                    'as':'activity_trackpoints'
+                }
+            },
+            #Separate the data to be able to use it
+            { "$unwind": "$activity_trackpoints" },
+            #Rejoin the data but now in the correct order
+            { "$group":
+                {
+                    "_id": {"_id":"$_id","user_id": "$user_id"},
+                    "date_time": {"$push": "$activity_trackpoints.date_time"}
+                } 
+            },
+            #Cleanly display data to be able to use it in python easily
+            {
+                "$project":
+                {
+                    "_id": "$_id._id",
+                    "user_id": "$_id.user_id",
+                    "date_time": "$date_time"
+                }
+            }
+            ]);
+        
+        user_invalids = dict()
+        five_minutes = 5*60
+
+        #Iterate over the activities and calculate check the trackpoints time
+        for activity in activities_with_date: 
+            #Calculate difference between one trackpoint and the previous 
+            previous_time = None
+            invalid = False
+            for trackpoint_time in activity["date_time"]: 
+                #If invalid, mark it and stop looping
+                if(previous_time is not None and (trackpoint_time - previous_time).total_seconds() >= five_minutes):
+                    invalid = True
+                    break
+                previous_time = trackpoint_time
+            #If invalid add the count to 
+            if(invalid):
+                if(activity["user_id"] in user_invalids):
+                    user_invalids[activity["user_id"]] += 1
+                else:
+                    user_invalids[activity["user_id"]] = 1
+        
+        #Put the results cleanly and order it by user id
+        user_invalids_orderes = dict(sorted(user_invalids.items()))
+        results = list()
+        for key in user_invalids_orderes:
+            results.append({
+                "user_id": key,
+                "number_invalid_activities": user_invalids[key]
+            })
+        #Print results
+        for result in results: pprint(result)
 
 
     def query_10(self):
